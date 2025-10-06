ab365107b9cb63077fb347231387cde77b183102
saaljiesma <69116453+saaljiesma@users.noreply.github.com>
2025-10-07 00:55:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -923,7 +923,7 @@ bot.onText(/\/frase/, (msg) => {
 // ======== COMANDO MANUAL /cancion ========
 bot.onText(/\/cancion/, (msg) => {
   const cancion = generarFrase(cancionesBuenosDias); // canciones es tu array de canciones con enlaces
-  bot.sendMessage(msg.chat.id, Aquí tienes tu canción del día: \n${cancion});
+  bot.sendMessage(msg.chat.id, 'Aquí tienes tu canción del día: \n${cancion}');
   console.log('Canción enviada con /cancion:', cancion);
 });
 console.log('Bot avanzado iniciado y listo. 🌞🎵');
